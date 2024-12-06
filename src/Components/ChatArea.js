782c6502143d2cfb3dfc89d6fa60d18fd9a9f56f
Ramu chiluveru <ramuchiluveru64@gmail.com>
2024-12-07 00:13:45 +0530

    chat_app_frontend

---
+++ src/Components/ChatArea.js
@@ -0,0 +1,165 @@
+import React, { useContext, useEffect, useRef, useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import MessageSelf from "./MessageSelf";
+import MessageOthers from "./MessageOthers";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Skeleton from "@mui/material/Skeleton";
+import axios from "axios";
+import { myContext } from "./MainContainer";
+
+function ChatArea() {
+
+  const url = "https://live-chat-server-5cc4.onrender.com/";
+
+  const lightTheme = useSelector((state) => state.themeKey);
+  const [messageContent, setMessageContent] = useState("");
+  const messagesEndRef = useRef(null);
+  const dyParams = useParams();
+  const [chat_id, chat_user] = dyParams._id.split("&");
+  // console.log(chat_id, chat_user);
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  const [allMessages, setAllMessages] = useState([]);
+  // console.log("Chat area id : ", chat_id._id);
+  // const refresh = useSelector((state) => state.refreshKey);
+  const { refresh, setRefresh } = useContext(myContext);
+  const [loaded, setloaded] = useState(false);
+  const sendMessage = () => {
+    // console.log("SendMessage Fired to", chat_id._id);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userData.data.token}`,
+      },
+    };
+    axios
+      .post(
+        `${url}message/`,
+        {
+          content: messageContent,
+          chatId: chat_id,
+        },
+        config
+      )
+      .then(({ data }) => {
+        console.log("Message Fired");
+      });
+  };
+
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userData.data.token}`,
+      },
+    };
+    axios
+      .get(`${url}message/` + chat_id, config)
+      .then(({ data }) => {
+        setAllMessages(data);
+        setloaded(true);
+      });
+  }, [refresh, chat_id, userData.data.token]);
+
+  if (!loaded) {
+    return (
+      <div
+        style={{
+          border: "20px",
+          padding: "10px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+        }}
+      >
+        <Skeleton
+          variant="rectangular"
+          sx={{ width: "100%", borderRadius: "10px" }}
+          height={60}
+        />
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            width: "100%",
+            borderRadius: "10px",
+            flexGrow: "1",
+          }}
+        />
+        <Skeleton
+          variant="rectangular"
+          sx={{ width: "100%", borderRadius: "10px" }}
+          height={60}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className={"chatArea-container" + (lightTheme ? "" : " dark")}>
+        <div className={"chatArea-header" + (lightTheme ? "" : " dark")}>
+          <p className={"con-icon" + (lightTheme ? "" : " dark")}>
+            {chat_user[0]}
+          </p>
+          <div className={"header-text" + (lightTheme ? "" : " dark")}>
+            <p className={"con-title" + (lightTheme ? "" : " dark")}>
+              {chat_user}
+            </p>
+            {/* <p className={"con-timeStamp" + (lightTheme ? "" : " dark")}>
+              {props.timeStamp}
+            </p> */}
+          </div>
+          <IconButton className={"icon" + (lightTheme ? "" : " dark")}>
+            <DeleteIcon />
+          </IconButton>
+        </div>
+        <div className={"messages-container" + (lightTheme ? "" : " dark")}>
+          {allMessages
+            .slice(0)
+            .reverse()
+            .map((message, index) => {
+              const sender = message.sender;
+              const self_id = userData.data._id;
+              if (sender._id === self_id) {
+                // console.log("I sent it ");
+                return <MessageSelf props={message} key={index} />;
+              } else {
+                // console.log("Someone Sent it");
+                return <MessageOthers props={message} key={index} />;
+              }
+            })}
+        </div>
+        <div ref={messagesEndRef} className="BOTTOM" />
+        <div className={"text-input-area" + (lightTheme ? "" : " dark")}>
+          <input
+            placeholder="Type a Message"
+            className={"search-box" + (lightTheme ? "" : " dark")}
+            value={messageContent}
+            onChange={(e) => {
+              setMessageContent(e.target.value);
+            }}
+            onKeyDown={(event) => {
+              if (event.code == "Enter") {
+                // console.log(event);
+                sendMessage();
+                setMessageContent("");
+                setRefresh(!refresh);
+              }
+            }}
+          />
+          <IconButton
+            className={"icon" + (lightTheme ? "" : " dark")}
+            onClick={() => {
+              sendMessage();
+              setRefresh(!refresh);
+            }}
+          >
+            <SendIcon />
+          </IconButton>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ChatArea;
